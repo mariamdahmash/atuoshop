dd6aabd91d5ec7af2bbd8d664086a7b45d24cbe6
Maryam Khaled <dahmashmariam@gmail.com>
2025-05-29 00:44:50 +0300

    edit2

--- auth.js
+++ auth.js
@@ -29,30 +29,28 @@ registerForm.addEventListener("submit", function (e) {
 });
 
 
-// 1. نربط بالفورم بتاع تسجيل الدخول
+
 const loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  // 2. ناخد البيانات اللي المستخدم كتبها
+
   const username = document.getElementById("loginUsername").value.trim();
   const password = document.getElementById("loginPassword").value.trim();
 
-  // 3. نقرأ كل المستخدمين من localStorage باستخدام الدالة اللي عملناها
   const users = loadFromStorage("users") || [];
 
-  // 4. ندور على المستخدم اللي بياناته متطابقة
   const matchedUser = users.find(user => user.username === username && user.password === password);
 
   if (matchedUser) {
-    // 5. لو موجود، نحفظه كـ مستخدم حالي
+    
     saveToStorage("loggedInUser", matchedUser);
 
     alert("You have successfully logged in.");
 
-    // 6. نروح بيه لصفحة المنتجات مثلاً
-    window.location.href = "products.html";
+    
+    window.location.href = "home.html";
   } else {
     alert("Incorrect username or password❌");
   }
